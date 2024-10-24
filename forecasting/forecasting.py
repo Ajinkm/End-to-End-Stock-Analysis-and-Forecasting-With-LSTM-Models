f79cf413e371cf5e838e3b71af1872af091f9324
Lekrey Jerel Jacob Laipiopa <lekrey.laipiopa29@gmail.com>
2024-10-24 13:37:43 +0700

    Update forecasting.py

--- forecasting/forecasting.py
+++ forecasting/forecasting.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import datetime
+import preprocessing
 import helper
 import preprocessing_model
 from sklearn.metrics import mean_absolute_error , mean_squared_error , mean_absolute_percentage_error , r2_score
